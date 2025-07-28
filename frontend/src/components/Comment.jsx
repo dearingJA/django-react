cef664af3762ca8bfe62f2f22da6c72476574cf4
Alexander Dearing <dearinja@lafayette.edu>
2025-07-28 16:45:03 -0400

    added comment like, dislike and reply buttons

--- frontend/src/components/Comment.jsx
+++ frontend/src/components/Comment.jsx
@@ -9,16 +9,31 @@ function Comment({ comment, onDelete, currentUser }) {
   }); 
 
 
-  return <div className="comment-container">
-    <p className="comment-author">{comment.author.username}</p>
-    <p className="comment-content">{comment.content}</p>
-    <p className="comment-date">{formattedDate}</p>
-    {currentUser.id === comment.author.id && (
-      <button className="delete-button" onClick= {() => onDelete(comment.id)}>
-        Delete
-      </button>
-    )}
-  </div>
+  return (
+    <div className="comment-container">
+      <div className="comment-info">
+        <p className="comment-author">{comment.author.username}</p>
+        <p className="comment-content">{comment.content}</p>
+        <p className="comment-date">{formattedDate}</p>
+      </div>
+      <div className="button-container">
+        <button className="comment-button like-button">
+          Like
+        </button>
+        <button className="comment-button dislike-button">
+          Dislike
+        </button>
+        <button className="comment-button reply-button">
+          Reply
+        </button>
+        {currentUser.id === comment.author.id && (
+          <button className="comment-button delete-button" onClick= {() => onDelete(comment.id)}>
+            Delete
+          </button>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default Comment
